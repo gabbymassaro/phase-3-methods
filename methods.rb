d15cfecc8841997e704b8c53495c106c2540acf6
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-08 10:07:25 -0400

    All JS functions are now Ruby methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
-# Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name = nil)
+  if name
+    puts "Hello, #{name}!"
+  else
+    puts "Hello, programmer!"
+  end
+end
+
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+
+def halve(num)
+  if Integer === num
+    return num / 2
+  else
+    return nil
+  end
+end
